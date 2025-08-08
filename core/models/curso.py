b383c3890e008646f6d0bb53855964562d380355
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2025-08-08 09:19:45 -0300

    FEAT: criar model de pessoa #20

--- core/models/curso.py
+++ core/models/curso.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Curso (models.Model):
     nome = models.CharField(max_length=120)
+
+    def __str__ (self):
+        return self.nome
--- core/models/tipo_setor.py
+++ core/models/tipo_setor.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class TipoSetor (models.Model):
     nome = models.CharField(max_length=120)
+
+    def __str__ (self):
+        return self.nome
