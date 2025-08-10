df43512032270f5a00a272a62bf13efa1630aced
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-08-10 00:32:59 -0300

    FEAT: serializer de curso criada [REFS: #24]

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,3 +2,4 @@ from .tipo_setor import TipoSetorSerializer
 from .setor import SetorSerializer
 from .categoria import CategoriaSerializer
 from .postagem import PostagemSerializer
+from .curso import CursoSerializer
