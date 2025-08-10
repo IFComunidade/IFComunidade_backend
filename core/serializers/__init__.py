1babb05968a238f9aac75547eab65798636735bd
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-08-10 00:36:51 -0300

    FEAT: serializer de opcao criado [REFS: #25]

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,3 +3,4 @@ from .setor import SetorSerializer
 from .categoria import CategoriaSerializer
 from .postagem import PostagemSerializer
 from .curso import CursoSerializer
+from .opcao import OpcaoSerializer
