6ecc24387a2fbc28069dfd6cb4196c9d538c9f1f
carloshenriqueok <ifcarloshenrique@gmail.com>
2025-08-10 22:11:04 -0300

    FEAT: models registradas no admin [REFS: #60]

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import Setor, Ocorrencia, Postagem, TipoSetor, Categoria, Opcao, Curso
 
-# Register your models here.
+admin.site.register(Setor)
+admin.site.register(Ocorrencia)
+admin.site.register(Postagem)
+admin.site.register(TipoSetor)
+admin.site.register(Categoria)
+admin.site.register(Opcao)
+admin.site.register(Curso)
+
+
+
